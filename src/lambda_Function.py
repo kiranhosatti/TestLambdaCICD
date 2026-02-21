3b3fb3e9dcf4a035bc906e4f9e7f9149b97ffa52
kiranhosatti <kiranp.hosatti@gmail.com>
2026-02-21 21:53:39 +0530

    name update

--- src/lambda_Function.py
+++ src/lambda_Function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello kiran')
+        'body': json.dumps('Hello Nikhil from NIE')
     }
